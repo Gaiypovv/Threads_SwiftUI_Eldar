065d4c323e0f216f644fa1c2cf89d30457447685
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-23 17:32:00 +0600

    Implemented the loginView

---
+++ Threads_SwiftUI_Eldar/Core/Activity/View/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  Threads_SwiftUI_Eldar
+//
+//  Created by Eldar Gaiypov on 23/11/23.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text("Activity")
+    }
+}
+
+#Preview {
+    ActivityView()
+}
