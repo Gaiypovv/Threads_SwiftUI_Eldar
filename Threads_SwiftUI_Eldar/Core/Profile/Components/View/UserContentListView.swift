bd983f09f23a1ebfd48508ae9ada09def51dcbdc
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-23 22:18:03 +0600

    Implemented the UserContentListView in  SwiftUI

---
+++ Threads_SwiftUI_Eldar/Core/Profile/Components/View/UserContentListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserContentListView.swift
+//  Threads_SwiftUI_Eldar
+//
+//  Created by Eldar Gaiypov on 23/11/23.
+//
+
+import SwiftUI
+
+struct UserContentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserContentListView()
+}
