cae7e77333a9859d2b269cf4d93baf3ca1f420db
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-23 15:13:13 +0600

    Initial Commit

---
+++ Threads_SwiftUI_Eldar/App/Threads_SwiftUI_EldarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_SwiftUI_EldarApp.swift
+//  Threads_SwiftUI_Eldar
+//
+//  Created by Eldar Gaiypov on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Threads_SwiftUI_EldarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
