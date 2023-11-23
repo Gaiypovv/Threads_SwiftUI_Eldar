1250a2e035c037f09c69248cf0ecaa4c7150d388
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-23 23:37:19 +0600

    Added ThreadsTabView, CreateThreadView

---
+++ Threads_SwiftUI_Eldar/Core/TabBar/ThreadsTabView.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadsTabView.swift
+//  Threads_SwiftUI_Eldar
+//
+//  Created by Eldar Gaiypov on 23/11/23.
+//
+
+import SwiftUI
+
+struct ThreadsTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadsTabView()
+}
