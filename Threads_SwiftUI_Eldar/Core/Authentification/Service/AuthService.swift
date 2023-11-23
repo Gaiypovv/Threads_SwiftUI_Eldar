018edeb9c9133dc8c6dc5d611d40d81132deb6c9
Eldar <114824844+Gaiypovv@users.noreply.github.com>
2023-11-23 22:06:41 +0600

    Added must have extensions files to the project

---
+++ Threads_SwiftUI_Eldar/Core/Authentification/Service/AuthService.swift
@@ -0,0 +1,8 @@
+//
+//  AuthService.swift
+//  Threads_SwiftUI_Eldar
+//
+//  Created by Eldar Gaiypov on 23/11/23.
+//
+
+import Foundation
---
+++ Threads_SwiftUI_Eldar/Utils/ViewModifiers/ThreadsTextFieldModifier.swift
@@ -0,0 +1,18 @@
+//
+//  ThreadsTextFieldModifier.swift
+//  Threads_SwiftUI_Eldar
+//
+//  Created by Eldar Gaiypov on 23/11/23.
+//
+
+import SwiftUI
+
+struct ThreadsTextFieldModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThreadsTextFieldModifier()
+}
